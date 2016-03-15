2f4d3f18a79d4cae95f8b4258439ddf30c3533c8
Miralas <miralas777@yandex.ru>
2016-03-15 21:00:53 +0300

    Add settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,6 @@ if defined?(FatFreeCRM::Application)
 
     # Do not dump schema after migrations.
     config.active_record.dump_schema_after_migration = false
-    config.secret_key_base = ENV["SECRET_KEY_BASE"]
+    config.secret_key_base = Rails.application.secrets.secret_key_base
   end
 end
