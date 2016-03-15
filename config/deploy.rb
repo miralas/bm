35819cd99fa6de0c223d67939dd5252c4ee2ac4e
Miralas <miralas777@yandex.ru>
2016-03-15 21:51:19 +0300

    fkjdgkldfg

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ set :linked_files, %w(config/database.yml config/settings.yml)
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 set :rvm_ruby_version, '2.2.1'
 
